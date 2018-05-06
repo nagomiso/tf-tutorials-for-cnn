b2a69fdace020ece641768aba2d649e50b232dfa
Nagomiso <daiseigi.giants@gmail.com>
2018-05-06 11:32:12 +0900

    Initial Commit.
    A Guide to TF Layers: Building a Convolutional Neural Network.

---
+++ cnn_mnist.py
@@ -0,0 +1,134 @@
+# coding: utf-8
+from functools import partial
+import sys
+
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+_print = partial(print, file=sys.stderr)
+
+
+def cnn_model_func(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    input_layer = tf.reshape(features['x'], [-1, 28, 28, 1])
+
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding='same',
+        activation=tf.nn.relu)
+
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(
+        inputs=conv1,
+        pool_size=[2, 2],
+        strides=2)
+
+    # Convolutional Layer #2 and Pooling Layer #2
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding='same',
+        activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(
+        inputs=conv2,
+        pool_size=[2, 2],
+        strides=2)
+
+    # Dense Layer
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    dense = tf.layers.dense(
+        inputs=pool2_flat,
+        units=1024,
+        activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+        inputs=dense,
+        rate=0.4,
+        training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout, units=10)
+
+    predictions = {
+        'classes': tf.argmax(input=logits, axis=1),
+        'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(
+            mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    loss = tf.losses.sparse_softmax_cross_entropy(
+        labels=labels, logits=logits)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op)
+
+        # Add evaluation metrics (for EVAL mode)
+        eval_metric_ops = {
+            'accuracy': tf.metrics.accuracy(
+                labels=labels, predictions=predictions['classes'])
+        }
+        return tf.estimator.EstimatorSpec(
+            mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+def main(unused_argv):
+    # Load training and eval data
+    (train_data, train_labels), (eval_data, eval_labels) = \
+            tf.keras.datasets.mnist.load_data()
+
+    # Type Casting
+    train_data = np.asarray(train_data, dtype=np.float32)
+    train_labels = np.asarray(train_labels, dtype=np.int32)
+    eval_data = np.asarray(eval_data, dtype=np.float32)
+    eval_labels = np.asarray(eval_labels, dtype=np.int32)
+
+    # Create the Estimator
+    mnist_classifier = tf.estimator.Estimator(
+        model_fn=cnn_model_func, model_dir='./mnist_cnn_model')
+
+    # Set up logging for predictons
+    # Log the values in the "Softmax" tensor with label "probabilities"
+    tensors_to_log = {'probabilities': 'softmax_tensor'}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=50)
+
+    # Train the model
+    train_input_func = tf.estimator.inputs.numpy_input_fn(
+        x={'x': train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True)
+    mnist_classifier.train(
+        input_fn=train_input_func,
+        steps=20000,
+        hooks=[logging_hook])
+
+    # Ealuate the model and print results
+    eval_input_func = tf.estimator.inputs.numpy_input_fn(
+        x={'x': eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_func)
+    _print(eval_results)
+
+
+if __name__ == '__main__':
+    tf.app.run()
